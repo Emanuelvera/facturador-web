168bcffd8e51cfbe1512a2a9e2a4ce0e82dedff8
matiassaunig <matias.saunig@mi.unc.edu.ar>
2025-10-25 15:41:33 -0300

    Add Cliente, Factura and Item class

---
+++ facturador-web/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace facturador_web.Models
+{
+    internal class Item
+    {
+    }
+}
